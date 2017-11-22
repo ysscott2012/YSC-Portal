430beba6e4ec01be2ccb1687b06937ea41433993
Yung-Sheng Chen <ysscott2012@gmail.com>
2017-11-22 00:48:33 -0500

    test

--- server/config/config.js
+++ server/config/config.js
@@ -1,6 +1,7 @@
 //  Local database setup (YS's local)
 process.env.PORT = 3000;
-process.env.MONGODB_URI = 'mongodb://localhost/authentication';
+//process.env.MONGODB_URI = 'mongodb://localhost/authentication';
 
 // process.env.MONGODB_URI = 'ys:ys@ec2-34-225-194-59.compute-1.amazonaws.com:27017/ysAwesome';
-// process.env.MONGODB_URI = 'mongodb://ec2-34-225-194-59.compute-1.amazonaws.com/authentication';
+process.env.MONGODB_URI = 'mongodb://ys:ys@ec2-34-225-194-59.compute-1.amazonaws.com:27017/authentication';
+ 
